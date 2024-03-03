4078819dbf4d4427430c82cd20f28f659aa82f41
Marlin Andrews <marlindandrews@gmail.com>
2024-03-03 13:17:58 +1100

    complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result ='';
+    //convert string into an array of characters
+    const stringCharacters = string.split("");
+    //reverse array StringCharacters
+    const reversedCharacters = stringCharacters.reverse();
+    for (let printChar = 0; printChar < reversedCharacters.length; printChar++){
+        result += stringCharacters[printChar];
+    }
+    return result;
+    
 };
 
 // Do not edit below this line

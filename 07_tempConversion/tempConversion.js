2bce63ee2f84f25e67fcd1533947369d054e8541
Marlin Andrews <marlindandrews@gmail.com>
2024-03-05 15:04:09 +1100

    complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahren){
+
+    const celsius =  (tempInFahren -32) * 5/9;
+    return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCels) {
+
+  const fahrenheit = (tempInCels * 9/5) + 32;
+  return Math.round(fahrenheit * 10) / 10
 };
+  
 
 // Do not edit below this line
 module.exports = {

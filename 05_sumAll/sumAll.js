453ba589178e25d64460ef8a74c947b4d4a7f1fc
Marlin Andrews <marlindandrews@gmail.com>
2024-03-05 14:04:23 +1100

    remove parseInt from logic as not neccisary

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    
+
+    // check for non-number parameters
+
+    // Check for negative numbers
+    if (startNumber < 0 || endNumber < 0) {
+        return 'ERROR';   
+    } 
+    
+
+    //determine smallNumber and largeNumber
+    const smallNumber = Math.min(startNumber, endNumber);
+    const largeNumber = Math.max(startNumber, endNumber);
+
+    //calculate the sum
+    let sum = 0;
+    for ( let i = smallNumber; i <= largeNumber; i++) {
+        sum +=i;
+    }
+
+    return sum;
 
 };
 
